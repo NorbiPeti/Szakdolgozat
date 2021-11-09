49480c5d67397283ac0f3e7a918dd1d6610f5225
NorbiPeti <szatmari.norbert.peter@gmail.com>
2021-11-09 01:55:57 +0100

    Add production build and deploy command

--- frontend/docker.sh
+++ frontend/docker.sh
@@ -1,3 +1,13 @@
+#!/usr/bin/env bash
+
+if [ $COMMAND == "deploy" ]; then
+    npm run buildProd
+    npm install -g firebase-tools
+    firebase login --no-localhost
+    firebase deploy
+    exit 0
+fi
+
 echo "Installing packages"
 npm install
 echo "Running application"
